2010b1e346647a6044115fe7091f47130ce34e71
saulcastillo97 <69877288+saulcastillo97@users.noreply.github.com>
2020-11-11 15:46:12 -0500

    Add files via upload

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -12,6 +12,7 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -21,8 +22,9 @@ def setup_db(app, database_path=database_path):
 
 '''
 Question
-
 '''
+
+
 class Question(db.Model):  
   __tablename__ = 'questions'
 
@@ -41,7 +43,7 @@ class Question(db.Model):
   def insert(self):
     db.session.add(self)
     db.session.commit()
-  
+
   def update(self):
     db.session.commit()
 
@@ -62,7 +64,7 @@ class Question(db.Model):
 Category
 
 '''
-class Category(db.Model):  
+class Category(db.Model):
   __tablename__ = 'categories'
 
   id = Column(Integer, primary_key=True)
