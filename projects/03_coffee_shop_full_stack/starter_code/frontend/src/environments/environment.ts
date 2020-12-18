f9b80b914997d4936e5f56ebfcb2a27dfc6e3053
saulcastillo97 <69877288+saulcastillo97@users.noreply.github.com>
2020-12-18 10:02:23 -0500

    Add files via upload

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'scfsnd.us.com', // the auth0 domain prefix//scfsnd.us.auth0.com
+    url: 'scfsnd.us', // the auth0 domain prefix//scfsnd.us.auth0.com
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'z6gAaOM7mtJtJISYz2U15qSsE0THmwwT', // the client id generated for the auth0 app
-    callbackURL: 'https://localhost:8100', // the base url of the running ionic application.
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
